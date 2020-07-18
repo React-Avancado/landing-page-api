ba8a9d616c86919a4ad125e5d2aa4c30e51936af
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-07-17 21:37:22 -0300

    Add CKEditor plugin

---
+++ plugins/wysiwyg/controllers/wysiwyg.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * wysiwyg.js controller
+ *
+ * @description: A set of functions called "actions" of the `wysiwyg` plugin.
+ */
+
+module.exports = {
+  /**
+   * Default action.
+   *
+   * @return {Object}
+   */
+
+  index: () => {},
+};
