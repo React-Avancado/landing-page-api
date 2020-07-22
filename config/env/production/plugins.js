cb45eb48b9aabf72c491c9eaba827e1584cd5e21
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-07-21 22:08:06 -0300

    Add AWS S3 Provider

---
+++ config/env/production/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
+      params: {
+        Bucket: env("AWS_BUCKET"),
+      },
+    },
+  },
+});
